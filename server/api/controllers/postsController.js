ccd6e468b01dd069e1bc9678567c2edc50258689
Jory Braun <braunjory@gmail.com>
2018-05-11 22:13:48 -0400

    resolves caching issues

--- server/api/controllers/postsController.js
+++ server/api/controllers/postsController.js
@@ -4,7 +4,8 @@ import {
   buildPostsFromCache,
   getPostCountFromCache,
   shouldFlush,
-  flushCache
+  flushCache,
+  getPostKeysFromCache
 } from '../services/cache'
 
 import { getTumblrPosts } from '../services/tumblr'
@@ -12,29 +13,25 @@ import { getTumblrPosts } from '../services/tumblr'
 export const getPosts = async (req, res, next) => {
   try {
     const offset = Number(req.query.offset) || 0;
-    const postKeys = Array.from(Array(5))
-      .map((x, i) => `post:${offset + i}`)
     const timeToLive = process.env.cache_ttl || 28800000
-
-    let hasCache = false
-    // if its the first call and ttl has expired then we should flush
-    if (offset === 0 && await shouldFlush(timeToLive)) {
-      await flushCache()
-    } else {
-      hasCache = await checkPostCache(postKeys)
-    }
-
-    if (!hasCache) {  
-      const blog = await getTumblrPosts(offset)
-      await hydratePostCache(blog, offset)
-    }
+    const postKeys = await getPostKeysFromCache(offset);
 
     const [count, posts] = await Promise.all([
       getPostCountFromCache(),
       buildPostsFromCache(postKeys)
     ])
 
-    res.json({count , posts})
+    getTumblrPosts(offset).then((blog) => {
+        if (count !== blog.count) {
+          hydratePostCache(blog, offset)
+        }
+    })
+
+    if (offset === 0 && await shouldFlush(timeToLive)) {
+      await flushCache()
+    }
+
+    res.json({count, posts})
   } catch (err) {
     console.error(err)
   }
